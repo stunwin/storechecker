b288fcb1a023dd6ebf457fdd2459facefd21a0fc
ICEMAN\stunwin <steve.unwin@gmail.com>
2024-05-03 16:08:48 -0700

    file input working. need error handling

--- geocodetest.py
+++ geocodetest.py
@@ -1,14 +1,33 @@
 import googlemaps
+import csv
+
+#look there might be a better way to obbuscate my api key from github, but i am a mere idiot merchant in wont of a wife
 import key
 
 gmaps = googlemaps.Client(key= key.key)
+list = []
 
-query = gmaps.geocode("2253 fairfax rd columbus oh 43221")
+with open("addresses.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        list.append(row)
+        row["lat"] = 0
+        row["lon"] = 0
 
-location = query[0]['geometry']['location']
+for store in list[5:100]:
+    query = gmaps.geocode(store["ADDRESS"] + store["CITY"] + store["STATE"] + store["ZIP"])
 
-lat = location["lat"]
-lon = location["lng"]
+    location = query[0]['geometry']['location']
+
+    store["lat"] = location["lat"]
+    store["lon"] = location["lng"]
 
 
-print(str(lat) + " " + str(lon))
+print(list)
+
+#write results file        
+with open("addresses.csv", "w", newline='') as file:
+    writer = csv.DictWriter(file, list[0].keys())
+    writer.writeheader()
+    for row in list:
+        writer.writerow(row)
